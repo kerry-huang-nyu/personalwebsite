fd2557389271417107e8d6d8bcb8796bd1402945
Kerry Huang <kerryhuang122@gmail.com>
2023-07-01 00:40:48 -0400

    fix the 50% issue

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //fix this and create a type of div for both the left and right pane in the future to minimize code 
+//this code  can def be optimized but the issue has been resolved 
 
 document.getElementById("leftpane").onmouseover = function() {
 	this.style.width = '90%' ;
@@ -12,18 +13,16 @@ document.getElementById("rightpane").onmouseover = function() {
     document.getElementById("leftpane").style.width = '10%';
 }
 
-
-//you need to change this to repond to on mouse out events on all pane elements 
-document.getElementsByClassName("pane").onmouseout = function(){
-    const collection = document.getElementsByClassName("pane");
-    for (let i = 0; i < collection.length; i ++){
-        collection[i].style.backgroundColor = "red";
-    }
-    document.getElementById("leftpane").style.width = '50%';
-    document.getElementById("rightpane").style.width = '50%';
-    this.style.backgroundColor = 'red';
+document.getElementById("rightpane").onmouseout = function() {
+	this.style.width = '50%' ;
     this.style.transitionDuration = '0.5s' ;
+    document.getElementById("leftpane").style.width = '50%';
 }
 
+document.getElementById("leftpane").onmouseout = function() {
+	this.style.width = '50%' ;
+    this.style.transitionDuration = '0.5s' ;
+    document.getElementById("rightpane").style.width = '50%';
+}
 
 
